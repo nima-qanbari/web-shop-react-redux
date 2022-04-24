c8010f10a653a9edcd6e35cea98d0f0a3ad4f7d8
nima <nimaqanbari1999@gmail.com>
2022-04-24 06:30:06 -0700

    add helper function

---
+++ src/helper/function.js
@@ -0,0 +1,25 @@
+
+const isInCart = (state , id) => {
+    const result = !!state.selectedItems.find(item => item.id === id)
+    return result
+}
+
+const quantityCount = (state , id) => {
+    const index = state.selectedItems.findIndex(item => item.id === id)
+    if(index === -1) {
+        return false
+    }else{
+        return state.selectedItems[index].quantity
+    }
+}
+
+
+const discount = (total) => {
+    if(total > 50) {
+       return (total * 0.2).toFixed(1)
+    }else {
+        return 0
+    }
+}
+
+export { isInCart , quantityCount, discount}
